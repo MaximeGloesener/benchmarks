3e3e39b2afce7ef113fe76039afdbc70493d776e
Maxime Gloesener <max.gleu@gmail.com>
2024-06-04 21:26:10 +0200

    init projet

---
+++ main.py
@@ -0,0 +1,42 @@
+# idea = iterate over all pytorch models and benchmark them
+# then create a streamlit app to display the results
+# models will be benchmarked on different devices -> in streamlite, it should be possible to choose device and see benchmark 
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torchvision import models
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+from benchmark import benchmark
+
+import torchvision.models as models
+
+Byte = 8
+KiB = 1024 * Byte
+MiB = 1024 * KiB
+GiB = 1024 * MiB
+
+# Iterate over all models 
+models_list = []
+for model_name in dir(models):
+    if model_name.islower() and model_name[0] != '_':
+        models_list.append(model_name)
+
+for model_name in models_list:
+    model_class = getattr(models, model_name)
+    if callable(model_class):
+        try:
+            model = model_class()
+            print(f"Successfully instantiated {model_name}")
+            fps_gpu, num_params, model_size, num_macs = benchmark(model, torch.randn(1, 3, 224, 224), n_warmup=50, n_test=200)
+            # write into file to display in streamlit
+            with open('GTX3060.txt', 'a') as f:
+                f.write(f'{model_name.capitalize()},{float(fps_gpu):.2f},{int(num_params/1e6)},{int(model_size/1e6)},{int(num_macs/1e6)}\n')
+        except Exception as e:
+            print(f"Failed to instantiate {model_name}: {e}")
+
---
+++ streamlite.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+
+# Title
+st.title('Pytorch Model Benchmarking')
+st.write('This app benchmarks all pytorch models on different devices')
+
+# Sidebar for selecting device
+st.sidebar.title('Settings')
+device = st.sidebar.selectbox('Device', ['GTX3060', 'RTX3090'])
+
+# Load data based on the selected device
+file_name = f"{device}.txt"
+try:
+    data = pd.read_csv(file_name, header=None, names=['Model', 'FPS', 'Nombre de paramètres (M)', 'Taille modèle (MB)', 'Nombre de MACs (M)'])
+    st.write(f"Displaying data for: {device}")
+    st.write(data)
+except FileNotFoundError:
+    st.error(f"No data available for {device}")
+
+# Optional: If you want to display the raw data as well
+# st.write(data)
